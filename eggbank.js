baeded048bffb55914ca33d9bc2bdd80ff672762
lexon <co.liang.ol@gmail.com>
2016-08-01 17:35:52 -0400

    Implemented eggbank blockchain bridge terminal I/F && commands interpreter

--- eggbank.js
+++ eggbank.js
@@ -9,99 +9,113 @@ var fs = require ('fs')
 
 var erisC = require('eris-contracts')
     , ndef = require('ndef')
-    , nfc  = require('nfc').nfc
     ;
 
-var appName = "eggbank"
-    , chainName = "eggchain"
-    ;
+var eggsContract;
+//------------------------------------------------------------------------------
+// Load eggbank contract with contract manager account.
+//------------------------------------------------------------------------------
+function loadEggContract(account) {
+    var appName = "eggbank"
+        , chainName = "eggchain"
+        ;
 
-var contractRoot = "./contract/"
-    , contractName = "deployEggbank"
-    , contractMngrName = "eggchain_developer_000"
-    ;
+    var contractRoot = "./contract/"
+        , contractName = "deployEggbank"
+        , contractMngrName = "eggchain_" + account
+        ;
 
-var erisdbURL = "http://localhost:1337/rpc";
+    var erisdbURL = "http://localhost:1337/rpc";
 
-//-$- get the abi and deployed data squared away -$-
-var contractData = require( contractRoot + appName + '/epm.json');
-var eggsContractAddress = contractData[contractName];
 
-var eggsAbi = JSON.parse( fs.readFileSync(contractRoot + appName + "/eggbank_abi/" 
-                          + eggsContractAddress) );
+    //-$- get the abi and deployed data squared away -$-
+    var contractData = require( contractRoot + appName + '/epm.json');
+    var eggsContractAddress = contractData[contractName];
 
-//-$- properly instantiate the contract objects manager using the erisdb URL
-// and the account data (which is a temporary hack) -$-
-var accountData = require(contractRoot + appName + "/accounts.json");
-var contractsManager = erisC.newContractManagerDev(erisdbURL, 
-                                            accountData[contractMngrName]);
+    var eggsAbi = JSON.parse( fs.readFileSync(contractRoot + appName + "/eggbank_abi/" 
+                + eggsContractAddress) );
 
-//-$- properly instantiate the contract objects using the abi and address -$-
-var eggsContract = contractsManager.newContractFactory(eggsAbi)
-    .at(eggsContractAddress);
+    //-$- properly instantiate the contract objects manager using the erisdb URL
+    // and the account data (which is a temporary hack) -$-
+    var accountData = require(contractRoot + appName + "/accounts.json");
+    var contractsManager = erisC.newContractManagerDev(erisdbURL, 
+            accountData[contractMngrName]);
 
+    //-$- properly instantiate the contract objects using the abi and address -$-
+    eggsContract = contractsManager.newContractFactory(eggsAbi)
+        .at(eggsContractAddress);
+
+}
+
+//------------------------------------------------------------------------------
+// Register egg carton
+// Args:
+//   uid(string): tag UID.
+//   cartonInfo(json): Carton data in json format.
+//   callback: Callback function.
+//------------------------------------------------------------------------------
 function registerEggCarton(uid, cartonInfo, callback) {
     eggsContract.registerCarton(uid, cartonInfo.name, cartonInfo.location, 
             new Date(cartonInfo.Expiration).getTime(), 
             new Date(cartonInfo['Boxed date']).getTime(),
             cartonInfo.NOE, 
             function(error, errCode) {
-        if (error) return;
+        if (error) {
+            console.log(error);
+            return;
+        }
         callback(errCode);
     });
 }
 
-
 //------------------------------------------------------------------------------
-// Read and process the RFID value
+//  Scan RFID/NFC tag
+//  Args:
+//    callback: callback function with {uid, json} dictionary obj as params.
+//    verbose(bool): Display tag details or not.
 //------------------------------------------------------------------------------
-function read(deviceID) {
-    var nfcdev = new nfc.NFC();
-    /* RFID data ready to read */
-    nfcdev.on('read', function(tag) {
-        //console.log(util.inspect(tag, { depth: null }));
-        if ((!!tag.data) && (!!tag.offset)) {
-            tlvs = nfc.parse(tag.data.slice(tag.offset))
-                //console.log(util.inspect(tlvs, { depth: null }))
-                if (!!tlvs && ('ndef' in tlvs[0])) {
-                    attachedData = tlvs[0].ndef[0].value;
-                    console.log("TAG: " + attachedData);
-                    if (attachedData == "eggs") {
-                        getValue(function (result) {
-                            curEggs = result.toNumber();
-                            console.log("Adding a dozen eggs")
-                            setValue(curEggs+12);
-                        });
+function scanTags(callback, verbose) {
+    var nfc = require('nfc').nfc;
 
-                    } else {
-                        console.log("Sorry, we don't accept " + attachedData);
-                    }
+    var devices = nfc.scan();
+    for (var deviceID in devices) {
+        var nfcdev = new nfc.NFC();                                                                             
+        nfcdev.start(deviceID);                                                                                 
+        nfc.readTag(nfcdev, function(tagdata) {                                                                 
+            // -$- Manufacture block -$-                                                
+            manudata = nfc.parseManufactureBlk(tagdata.slice(0, 16));                   
+            if(verbose) {
+                console.log("uid: " + nfc.bufToHexString(new Buffer(manudata.uid), ' '));       
+                console.log("check byte 0: " + ('0' + manudata.cb0.toString(16)).substr(-2));
+                console.log("check byte 1: " + ('0' + manudata.cb1.toString(16)).substr(-2));
+                console.log("internal: " + ('0' + manudata.internal.toString(16)).substr(-2));
+                console.log("lock bytes: " + nfc.bufToHexString(new Buffer(manudata.lock), ' '));
+                console.log("capability container: " + nfc.bufToHexString(new Buffer(manudata.cc), ' '));
+                console.log();                                                                                      
+            }
 
-                }
-
+            // -$- Read JSON data -$-                                                   
+            var tlvs = nfc.parse(tagdata.slice(16));                                                            
+            tagval = tlvs[0].ndef[0].value;                                                                     
+            if(verbose) console.log("Original text: " + tagval);                                                            
+            if (!isEggTag(tagval)) {                                                                            
+                console.log("It's not an egg carton tag!!!");
+                nfcdev.stop();                                                                                  
+                return;                                                                                         
+            }                                                                                                   
+        
+            var infoObj = JSON.parse(tagval);                                                                       
+            var cartonUID = nfc.bufToHexString(new Buffer(manudata.uid), '');
             nfcdev.stop();
-
-        } else {
-            console.log("Hold tag longer on the RFID reader.")
-        }
-
-    });
-
-    /* RFID/NFC error callback. */
-    nfcdev.on('error', function(err) {
-        console.log(util.inspect(err, { depth: null }));
-    });
-
-    /* RFID/NFC stopped, clean up. */
-    nfcdev.on('stopped', function() {
-        //console.log('stopped');
-        console.log('');
-    });
-
-    nfcdev.start(deviceID)
-        console.log("Waiting for eggs...");
+            callback({
+                'uid': cartonUID, 
+                'json': infoObj
+            });
+        });
+    }
 }
 
+// -$- Eggbank error definitions -$-
 var NO_ERROR = 0
     , RESOURCE_NOT_FOUND = 1001
     , RESOURCE_ALREADY_EXISTS = 1002
@@ -117,7 +131,7 @@ errors[ACCESS_DENIED] = "Access denied";
 errors[ARRAY_INDEX_OUT_OF_BOUNDS] = "Index out of bounds";
 
 //------------------------------------------------------------------------------
-// Start egg server to verify eggs.
+// Start egg server to serve REST requests.
 //------------------------------------------------------------------------------
 function startEggServer() {
     var restify = require('restify');                                                                           
@@ -239,6 +253,9 @@ function startEggServer() {
     console.log("server running at 0.0.0.0:56659");
 }
 
+//------------------------------------------------------------------------------
+// Determine if a tag is a egg tag or not.
+//------------------------------------------------------------------------------
 function isEggTag(tagval) {                                                     
     try {                                                                       
         jsonObj = JSON.parse(tagval);                                           
@@ -249,11 +266,174 @@ function isEggTag(tagval) {
     return jsonObj.hasOwnProperty('name') && jsonObj.name === "eggs";           
 }    
 
+//------------------------------------------------------------------------------
+// ebb *register* command handler
+//------------------------------------------------------------------------------
+function ebbRegister(r, token) {
+    var finish = token;
+    var verbose = r.flags.v;
+    verbose = typeof verbose !== 'undefined' ?  verbose : false;
+    var tag = scanTags(function (tag) {
+        if (!!tag) {
+            registerEggCarton(tag.uid, tag.json, function(error) {
+                if (error == NO_ERROR) {
+                    ebbPrintResult("Congrats! Registered egg carton  " + tag.uid);
+                } else{
+                    ebbPrintResult("ERROR registering " + tag.uid + ". " + errors[error]);
+                }           
+                finish();
+                return;
+            });
+        } else {
+            finish();
+        }
+    }, verbose);
+
+}
+
+//------------------------------------------------------------------------------
+// ebb *transfer* command handler
+//------------------------------------------------------------------------------
+function ebbTransfer(r , token) {
+    var finish = token;
+    var eggid = r.args[0];
+    var to = r.parameters.target;
+    
+    eggsContract.transferCarton(eggid, to, function (error, result) {
+        if (error) {
+            console.log(error);
+        }
+        if (result == NO_ERROR) {
+            ebbPrintResult("Egg carton " + eggid + " has been transferred to " + to);
+        } else {
+            ebbPrintResult("ERROR transferring egg carton " + eggid + ". " + errors[result]);
+        }
+        finish();
+        return;
+    });   
+}
+
+//------------------------------------------------------------------------------
+// ebb *dispose* command handler
+//------------------------------------------------------------------------------
+function ebbDispose(r, token) {
+    var finish = token;
+    var eggid = r.args[0];
+    eggsContract.disposeCarton(eggid, function (error, result) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (result == NO_ERROR) {
+                ebbPrintResult("Egg carton " + eggid + " has been disposed.");
+            } else {
+                ebbPrintResult("ERROR disposing egg carton " + eggid + ". " + errors[result]);
+            }
+        }
+        finish();
+        return;
+    });
+
+}
+
+//------------------------------------------------------------------------------
+// ebb *help* command heandler
+//------------------------------------------------------------------------------
+function ebbHelp(r, token) {
+    ebbPrintResult(r.help());
+    token();
+}
+
+//
+function ebbPrintResult(result) {
+    console.log();
+    console.log(result);
+    console.log();
+}
+
+//------------------------------------------------------------------------------
+// Start eggbank blockchain bridge terminal app
+//------------------------------------------------------------------------------
+function startEggTerminal() {
+    var lineparser = require('lineparser');
+    var meta = {
+        program: "ebb",
+        name: "Eggbank Blockchain Bridge",
+        version: "0.0.1",
+        subcommands: ['dispose', 'help', 'register', 'transfer'],
+        options: {
+            flags: [
+                // -$- short_name, name, description -$-
+                [ 'h', 'help', 'print program usage' ],
+                [ 'v', 'verbose', 'print detailed information' ]
+            ],
+            parameters: [
+                // -$- short_name, name, description, default_value -$-
+                ['t', 'target', 'The target bilateral command', '590244C2F0D8A3D09B68802B4A206C842FA0B864']
+            ]
+        },
+        usages: [
+            // -$- subcommand, options, positional-arguments, description, handler -$-
+            ['register', ['[v]'], null, 'Register egg carton to eggchain.', ebbRegister],
+            ['transfer', ['[target]'], ['uid'], 'Transfer egg carton.', ebbTransfer],
+            ['dispose', null, ['uid'], 'Dispose egg carton.', ebbDispose],
+            ['help', null, null, 'help', ebbHelp],
+            [null, null, null, 'help', ebbHelp]
+        ]
+    };
+
+    var optparser = lineparser.init(meta);
+    var argsparser = require('shell-quote').parse;
+    var prompt = require('prompt-sync')({
+        history: require('prompt-sync-history')('prompt_history.txt', 100)
+    });
+    var sleep = require('sleep-async')();
+    
+    function ebbTerminal() {
+        var cmdline = prompt('ebb > ', 'help');
+        if (cmdline == 'exit') {
+            prompt.history.save();
+            return;
+        }
+        
+        // -$- Synchronizing of callback function and terminal -$-
+        // TODO[fix]: `sleepWithCondition` return delayed on second pass of recursion.
+        var finished = false;
+        function finish() {
+            finished = true;
+        }
+        function hasFinished() {
+            return finished ? true : false;
+        }
+
+        try {
+            optparser.parse(argsparser(cmdline), finish);
+            var timeoutopts = {
+                sleep: 120000,  // 5min
+                interval: 500   // 1/2sec
+            };
+            sleep.sleepWithCondition(hasFinished, timeoutopts, ebbTerminal);
+        } catch (e) {
+            ebbTerminal();
+        }
+    }
+    ebbTerminal();
+}
+
 //-$- Main entry -$-
 var args = process.argv.slice(2);
-if (args.length > 0 &&  args[0] == "server") {
-    startEggServer();
+var account = typeof args[1] !== 'undefined' ? args[1] : 'developer_000';
+loadEggContract(account);
+
+if (args.length > 0) {
+    if (args[0] == "server") {
+        startEggServer();
+    }
+    else if (args[0] == "terminal") {
+        startEggTerminal();
+    }
 } else {
+    var nfc  = require('nfc').nfc;
+
     var devices = nfc.scan();
     for (var deviceID in devices) {
          //read(deviceID);
